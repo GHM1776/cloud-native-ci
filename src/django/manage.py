c244837a543b4b1118a534e66118b079fb45615b
GHM1776 <gregory.mcdonald1988@gmail.com>
2025-03-21 16:08:18 +0000

    Add test files and proper structure for CI pipeline

--- src/django/manage.py
+++ src/django/manage.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        pass
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed?"
+        ) from exc
     execute_from_command_line(sys.argv)
